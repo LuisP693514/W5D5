c176772d1674a4145aeefd01ebbafadcf28552ae
Logan Hartman <loganchart@gmail.com>
2022-12-09 16:04:33 -0500

    EOD commit

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -6,6 +6,8 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
+  Movie.select(:id, :title, :score)
+  .where(score: 2..3)
 
 end
 
@@ -20,6 +22,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
+
+  Movie.joins(:actors)
+    .where(actors: {name: 'Harrison Ford'})
+    .where.not(castings: {ord: 1})
+    .select(:id, :title)
 
 end
 
@@ -37,6 +44,14 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the largest casts (i.e., most
   # actors).
+
+  Movie
+    .joins(:castings)
+    .group('movies.id')
+    .order('COUNT(actor_id) DESC')
+    .select(:id, :title)
+    .limit(3)
+
   
 end
 
@@ -54,6 +69,11 @@ def directed_by_one_of(them)
   # Find the id and title of all the movies directed by one of 'them'.
   
   # Note: Directors appear in the 'actors' table.
+  # p them
+  Movie
+    .joins(:director)
+    .where(actors: {name: them})
+    .select(:id, :title)
 
 end
 
@@ -68,5 +88,9 @@ def movie_names_before_1940
   # improve performance for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie
+    .where('yr < 1940')
+    .pluck(:title)
 
 end
--- movie_buff/02_queries.rb
+++ movie_buff/02_queries.rb
@@ -1,18 +1,33 @@
+require "byebug"
+
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
+  Movie
+    .where(yr: 1980..1989, score: 3..5)
+    .select(:id, :title, :yr, :score)
   
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
+  Movie
+    .group(:yr)
+    .having('MAX(score) < 8')
+    .pluck(:yr)
   
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+  # id = Movie.find_by(title: title)
   
+  Actor
+    .joins(:movies)
+    .where(movies: {title: title})
+    .select(:id, :name)
+    .order('castings.ord')  
 end
 
 def vanity_projects
@@ -20,11 +35,25 @@ def vanity_projects
   # starring actor. Show the movie id, title, and director's name.
 
   # Note: Directors appear in the 'actors' table.
+  Movie
+    .joins(:actors)
+    .where('castings.actor_id = movies.director_id')
+    .where(castings: {ord: 1})
+      # castings: {actor_id: :director_id,  ord: 1})
+    .select(:id, :title, :name)
   
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name, and number of supporting roles.
+
+  Actor
+    .joins(:castings)
+    .where.not(ord: 1)
+    .group(:actor_id)
+    .select('id, name, COUNT (*)' )
+    .order('actor_id desc')
+    .limit(2)
   
 end
